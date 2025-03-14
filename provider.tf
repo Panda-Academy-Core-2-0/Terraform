500bcf20a00cd536c6efb162620ac40ff2b74904
PandaAcademy <kontakt@pandait.pl>
2025-03-14 15:47:36 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     # bucket = "<name>-<surname>-panda-devops-core-8"
-    bucket = "testingpandabucket2"
+    bucket = "testingpandabucket111"
     key = "infra/terraform.state"
     region = "us-east-1"
   }
